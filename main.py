0b79ca6b7cca18f77040f7b7d996f7390f03c67f
Falkhan <falkhan.mage@gmail.com>
2018-04-19 19:54:58 +0200

    Add comments

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def get_price(r):
     return r.json()["USD"]["last"]
 
 def main():
+    # Get a request from an API
     r = requests.get("https://blockchain.info/ticker")
-    print(r.status_code)
+    if (r.status_code == 200):
+        print("Success! Site is up.")
+    elif (r.status_code == 400):
+        print("Bad request")
     data = r.json()
     last_price = data["USD"]["last"]
     stream(last_price)
@@ -17,14 +21,19 @@ def stream(price):
     last_price = price
     time.sleep(1)
     r = requests.get("https://blockchain.info/ticker")
-    if(get_price(r) > last_price):
-        print("UP: "+ str(get_price(r)), "BY "+ str(get_price(r) - last_price))
-    elif (get_price(r) < last_price):
-        print("DOWN: "+ str(get_price(r)), "BY "+str(last_price-get_price(r)))
-    else:
-        print("SAME: " + str(last_price))
-    last_price = get_price(r)
-    stream(last_price)
+
+    ## If the request get an OK resposne, the program continues
+    if (r.status_code == 200):
+
+        if(get_price(r) > last_price):
+            print("UP: "+ str(get_price(r)), "BY "+ str(get_price(r) - last_price))
+        elif (get_price(r) < last_price):
+            print("DOWN: "+ str(get_price(r)), "BY "+str(last_price-get_price(r)))
+        else:
+            print("SAME: " + str(last_price))
+
+        last_price = get_price(r)
+        stream(last_price)
 
 if __name__ == '__main__':
     main()
