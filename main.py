527d4c877e43fdaca418a3d4e1e46c2060f4e1ca
Falkhan <falkhan.mage@gmail.com>
2018-04-19 19:51:22 +0200

    feat: change the while loop to a recursive function

--- main.py
+++ main.py
@@ -10,18 +10,21 @@ def main():
     r = requests.get("https://blockchain.info/ticker")
     print(r.status_code)
     data = r.json()
-    #print(data["USD"])
     last_price = data["USD"]["last"]
-    while True:
-        time.sleep(1)
-        r = requests.get("https://blockchain.info/ticker")
-        if(get_price(r) > last_price):
-            print("UP: "+ str(get_price(r)), "BY "+ str(get_price(r) - last_price))
-        elif (get_price(r) < last_price):
-            print("DOWN: "+ str(get_price(r)), "BY "+str(last_price-get_price(r)))
-        else:
-            print("SAME: " + str(last_price))
-        last_price = get_price(r)
+    stream(last_price)
+
+def stream(price):
+    last_price = price
+    time.sleep(1)
+    r = requests.get("https://blockchain.info/ticker")
+    if(get_price(r) > last_price):
+        print("UP: "+ str(get_price(r)), "BY "+ str(get_price(r) - last_price))
+    elif (get_price(r) < last_price):
+        print("DOWN: "+ str(get_price(r)), "BY "+str(last_price-get_price(r)))
+    else:
+        print("SAME: " + str(last_price))
+    last_price = get_price(r)
+    stream(last_price)
 
 if __name__ == '__main__':
     main()
