cf83273db09ee656871b51be73c675a5dad90c06
Falkhan <falkhan.mage@gmail.com>
2018-04-19 19:45:51 +0200

    feat: Add basic functions - get requests every second to see if the price of BTC changes

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import requests
+import json
+import time
+
+
+def get_price(r):
+    return r.json()["USD"]["last"]
 
 def main():
     r = requests.get("https://blockchain.info/ticker")
     print(r.status_code)
     data = r.json()
-    print(data["USD"])
+    #print(data["USD"])
     last_price = data["USD"]["last"]
-    print(last_price)
-
+    while True:
+        time.sleep(1)
+        r = requests.get("https://blockchain.info/ticker")
+        if(get_price(r) > last_price):
+            print("UP: "+ str(get_price(r)), "BY "+ str(get_price(r) - last_price))
+        elif (get_price(r) < last_price):
+            print("DOWN: "+ str(get_price(r)), "BY "+str(last_price-get_price(r)))
+        else:
+            print("SAME: " + str(last_price))
+        last_price = get_price(r)
 
 if __name__ == '__main__':
     main()
